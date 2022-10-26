2a5606b37c588b1694c840a3e7f30382f46ff5a7
Radek Svanda <radek.svanda@elder.cz>
2022-10-26 10:40:00 +0200

    Unifies colors between bash and firefox containers

--- aws-bash/_aws-bash-prompt.sh
+++ aws-bash/_aws-bash-prompt.sh
@@ -5,11 +5,23 @@ __aws_bash_prompt () {
       tmpenv="$(AWS_VAULT="" aws-session $tmpvault)"
       export `echo $tmpenv`
     fi
+
+    # terminal colors:
+    # 0 	black
+    # 1 	red
+    # 2 	green
+    # 3 	yellow
+    # 4 	blue
+    # 5 	magenta
+    # 6 	cyan
+    # 7 	white    
+
     OFFSET=$(( $(cksum -a crc <<< $AWS_VAULT | awk '{print $1}') % 5 ))
     FG=$(( $OFFSET + 32 ))
     BG=$(( $OFFSET + 42 ))
     PS1="$(printf "\[\033[1;%s;1;%sm\]<aws:%s>\[\033[0m\]%s" $FG $BG $AWS_VAULT) $PS1"
   fi
+}
 
 if [[ ! $PROMPT_COMMAND == *"__aws_bash_prompt"* ]]; then
   PROMPT_COMMAND="${PROMPT_COMMAND};__aws_bash_prompt"
